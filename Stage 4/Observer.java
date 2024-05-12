297ea23713d45c2f5128063d4bca60b997dc6847
Rahafmg12 <132592912+Rahafmg12@users.noreply.github.com>
2024-05-12 21:00:33 +0300

    Update Observer.java

--- Stage 4/Observer.java
+++ Stage 4/Observer.java
@@ -1,23 +1,35 @@
-package coffee;
-
-import java.util.Observable;
-import java.util.Observer;
+import java.util.ArrayList;
+import java.util.List;
 
 interface CoffeeMachineObserver {
     void update(int remainingCups);
 }
 
-class CoffeeMachine extends Observable {
-   private int remainingCups;
+class CoffeeMachine {
+    private int remainingCups;
+    private List<CoffeeMachineObserver> observers = new ArrayList<>();
 
     public int getRemainingCups() {
-       return remainingCups;
+        return remainingCups;
     }
 
     public void setRemainingCups(int remainingCups) {
         this.remainingCups = remainingCups;
-        setChanged();
-        notifyObservers(remainingCups);
+        notifyObservers();
+    }
+
+    public void addObserver(CoffeeMachineObserver observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(CoffeeMachineObserver observer) {
+        observers.remove(observer);
+    }
+
+    private void notifyObservers() {
+        for (CoffeeMachineObserver observer : observers) {
+            observer.update(remainingCups);
+        }
     }
 }
 
@@ -44,16 +56,31 @@ class Order implements CoffeeMachineObserver {
     }
 }
 
+class Coffee {
+    private String name;
+    private double price;
+
+    public Coffee(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-
         CoffeeMachine coffeeMachine = new CoffeeMachine();
 
         Coffee espresso = new Coffee("Espresso", 1.5);
         Coffee latte = new Coffee("Latte", 2.5);
         Coffee cappuccino = new Coffee("Cappuccino", 3.0);
-
-        
 
         Order order1 = new Order(espresso, 2);
         Order order2 = new Order(latte, 1);
